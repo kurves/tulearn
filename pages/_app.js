1d374f360d919efd8ef438d64ab8ea5fc19c5945
kurves <84717663+kurves@users.noreply.github.com>
2022-04-02 13:04:35 +0300

    updated package json

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,13 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
 import { Worker } from "@react-pdf-viewer/core";
 
-
-
 function MyApp({ Component, pageProps }) {
-  return (
-  <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.13.216/build/pdf.worker.min.js">
- <Component {...pageProps} />
+	return (
+		<Worker workerUrl="https://unpkg.com/pdfjs-dist@2.13.216/build/pdf.worker.min.js">
+			<Component {...pageProps} />
+		</Worker>
+	);
+}
 
-  </Worker>
- 
-  )}
-
-
-
-
-
-
-
-export default MyApp
+export default MyApp;
