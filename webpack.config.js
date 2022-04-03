5a9f9082e8e3cbf03c69c1f2c96de29ee9b2457d
kurves <84717663+kurves@users.noreply.github.com>
2022-04-03 16:56:33 +0300

    update deendencies

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+    context:__dirname,
+	entry: {
+		main: "/index.js",
+		"pdf.worker": path.join(
+			__dirname,
+			"./node_modules/pdfjs-dist/build/pdf.worker.js"
+		),
+	},
+
+	output: {
+		path: path.join(__dirname, "dist"),
+		filename: "[name].bundle.js",
+	},
+
+	plugins: [
+		new webpack.NormalModuleReplacementPlugin(/^pdfjs-dist$/, (resource) => {
+			resource.request = path.join(
+				__dirname,
+				"./node_modules/pdfjs-dist/webpack"
+			);
+		}),
+	],
+};
